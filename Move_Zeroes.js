747866f0a86281da9c0e1f02fb0669dfa89adba7
Madhurya Sarkar <madhuryasarkar1993@gmail.com>
2017-01-09 00:53:56 -0600

    Update Move_Zeroes.js

--- Move_Zeroes.js
+++ Move_Zeroes.js
@@ -3,21 +3,20 @@
 
 For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0]. */
 
- var moveZeros = function(nums){
-  var i = 0,j = 1, len = nums.length;
-  if(nums.length < 1){return nums[0] === 0 ? [nums[1],0] : nums;}
   
-  for(i,j; j < len; ){
-    if(nums[i] === 0 && nums[j] !== 0){
-      var tmp = nums[j];
-      nums[j] = nums[i];
-      nums[i] = tmp;
-      i++; j++;
-      continue;
-    }
-    while(nums[i] !== 0 && i < len){ i++; }
-    j = i + 1;
-    while (nums[j] === 0 && j < len ) { j++; }
-  }
-  console.log(nums);
+  Array.prototype.move = function(from,to){
+  this.splice(to,0,this.splice(from,1)[0]);
+  return this;
 };
+var moveZeroes = function(nums) {
+
+var result;
+  
+  for(var i=0; i<nums.length;i++){
+    if(nums[i] === 0){
+     nums.move(i,nums.length-1);
+    }
+  }
+  return nums;
+};
+moveZeroes([0,1,0,5,8,7,0,1,5]);
